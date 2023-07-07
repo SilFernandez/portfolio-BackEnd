ad53050aac5581bb76a9e8e7e94ba52e5bb3cea5
SilFernandez <102128832+SilFernandez@users.noreply.github.com>
2023-07-07 13:51:54 -0300

    initializer

---
+++ src/test/java/com/portfoliosf/sf/SfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliosf.sf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
